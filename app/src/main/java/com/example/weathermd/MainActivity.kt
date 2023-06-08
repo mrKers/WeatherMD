455c1854cf2056fdd4c2509bc06cbdbf48a9e7fa
kers2 <kersick21@gmail.com>
2023-06-08 15:25:08 +0300

    Add id elements in main view

--- app/src/main/java/com/example/weathermd/MainActivity.kt
+++ app/src/main/java/com/example/weathermd/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.weathermd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         //dasmdknaksldna
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        main_city_name_tv.text = "Moskow"
+        main_data_tv.text = "8 Jule"
+        main_weather_condition_icon.setImageResource(R.drawable.outline_wb_sunny_24)
+        main_temp.text = "25\u00B0"
+        main_temp_min_tv.text = "19"
+        main_temp_max_tv.text = "25"
+        main_temp_sr_tv.text = "22"
+        main_weather_image.setImageResource(R.mipmap.cloud)
+        main_pressure_mu_tv.text = "1023hPa"
+        main_humidity_mu_tv.text = "80%"
+        main_wind_speed_mu_tv.text = "5 m/s"
+        main_sunrise_mu_tv.text = "4:20"
+        main_sunset_mu_tv.text = "21:30"
     }
 }
